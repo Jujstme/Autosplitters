d42c51059f3be7e542d2285cf34133b76dfc8c16
Jujstme <just.tribe@gmail.com>
2021-09-18 20:11:57 +0200

    Add files via upload

--- Sonic Colors Ultimate/SonicColorsFactory.cs
+++ Sonic Colors Ultimate/SonicColorsFactory.cs
@@ -14,9 +14,9 @@ namespace LiveSplit.SonicColors
         public string Description => "Automatic splitting and IGT calculation for Sonic Colors Ultimate";
         public ComponentCategory Category => ComponentCategory.Control;
         public string UpdateName => this.ComponentName;
-        public string UpdateURL => "http://fatalis.pw/livesplit/update/";
+        public string UpdateURL => "https://raw.githubusercontent.com/Jujstme/Autosplitters/master/Sonic%20Colors%20Ultimate/";
         public Version Version => Assembly.GetExecutingAssembly().GetName().Version;
-        public string XMLURL => this.UpdateURL + "Components/update.LiveSplit.Dishonored.xml";
+        public string XMLURL => this.UpdateURL + "Components/update.LiveSplit.SonicColors.xml";
         public IComponent Create(LiveSplitState state)
         {
             return new SonicColorsComponent(state);
